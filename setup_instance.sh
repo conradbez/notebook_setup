9dfee595e7f219de3fc09f17694791ad8944b099
Ubuntu <ubuntu@ip-172-31-22-36.us-west-2.compute.internal>
2020-03-03 01:40:47 +0000

    initial

---
+++ setup_instance.sh
@@ -0,0 +1,11 @@
+mv .jupyter /home/ubuntu/.jupyter
+
+python3 -m virtualenv .venv
+
+source .venv/bin/activate
+
+pip install -r requirements.txt
+
+echo which python
+
+ipython kernel install --user --name=.venv
